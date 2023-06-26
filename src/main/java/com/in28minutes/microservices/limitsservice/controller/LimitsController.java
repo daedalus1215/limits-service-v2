c8644ed064f76613f949db567e108b6adf1f9893
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-06-26 10:47:43 -0400

    Have Limits come off a config - prior to spring cloud config

--- src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
@@ -1,13 +1,18 @@
 package com.in28minutes.microservices.limitsservice.controller;
 
 import com.in28minutes.microservices.limitsservice.Limits;
+import com.in28minutes.microservices.limitsservice.config.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
